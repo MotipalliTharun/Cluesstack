6ac43654f535b5b239cf2fe86e192813e6c034e1
Tharun <motipallitharun@gmail.com>
2025-11-30 19:25:36 -0500

    s1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: { 
     sourcemap: false, 
     target: "es2020",
-    minify: "terser",
+    minify: "esbuild",
     terserOptions: {
       compress: {
         drop_console: true,
